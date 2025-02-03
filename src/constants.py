1ebda23d6423e8ca28e57cbde69123a15a568901
wlyle473 <wlyle473@gmail.com>
2025-02-03 13:26:06 +0000

    refactor: Added file for constants.

---
+++ src/constants.py
@@ -0,0 +1,9 @@
+# Paths
+MAIN_DATA_DIR = "data"
+MAIN_DATA_FILE = "CW1_train.csv"
+DATA_PATH = f"{MAIN_DATA_DIR}/{MAIN_DATA_FILE}"
+
+# Constants
+EPS = 1e-8
+REPRODUCIBILITY_SEED = 42
+NUM_FOLDS = 5
