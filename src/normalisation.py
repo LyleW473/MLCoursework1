f8d094755d1a97c5348a8ca08925e020c8ca364a
wlyle473 <wlyle473@gmail.com>
2025-02-17 17:36:16 +0000

    fix: Issue with test data not being normalised using training data.

--- src/normalisation.py
+++ src/normalisation.py
@@ -1,14 +1,37 @@
 import pandas as pd
+from typing import Union
 
 class Normaliser:
-    def standardise(self, data:pd.DataFrame) -> pd.DataFrame:
+    def calculate_mean(self, data:pd.DataFrame) -> pd.Series:
+        """
+        Calculates the mean of a DataFrame.
+
+        Args:
+            data (pd.DataFrame): The data to calculate the mean of.
+        """
+        mean = data.mean()
+        return mean
+
+    def calculate_std(self, data:pd.DataFrame) -> pd.Series:
+        """
+        Calculates the standard deviation of a DataFrame.
+
+        Args:
+            data (pd.DataFrame): The data to calculate the standard deviation of.
+        """
+        std = data.std()
+        return std
+    
+    def standardise(self, data:pd.DataFrame, mean:Union[pd.Series, None]=None, std:Union[pd.Series, None]=None) -> pd.DataFrame:
         """
         Standardises a DataFrame by subtracting the mean and dividing by the standard deviation.
 
         Args:
             data (pd.DataFrame): The data to standardise.
         """
-        mean = data.mean()
-        std = data.std()
+        if mean is None:
+            mean = data.mean()
+        if std is None:
+            std = data.std()
         data = (data - mean) / (std + 1e-8)
         return data
